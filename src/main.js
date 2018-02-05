9532374d4d8be83d3c19bc9f938e4453e6dbb42c
Ouraborus <1222eca@gmail.com>
2018-02-05 14:15:45 -0500

    First Commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// import {Test} from './components/test.js'
+import './components/sass/main.scss'
+import { NavBar } from './components/navBar/navBar'
+
+/* eslint-disable */
+    new NavBar('.navbar')
+/* eslint-enable */
