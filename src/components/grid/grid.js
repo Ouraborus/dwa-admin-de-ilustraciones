d3f5bf7a1639231462a88bdef18a25c58a0f271a
Ouraborus <1222eca@gmail.com>
2018-02-12 20:09:58 -0500

    Working with firebase, trying to load the data from users

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -4,14 +4,14 @@ export class Grid {
   constructor (selector, cards) {
     this.node = document.querySelector(selector)
     this.elements = {}
-    this.elements.cards = []
+    this.cards = []
 
     this.loadCards(cards)
   }
 
   loadCards (cards) {
     cards.forEach(element => {
-      this.elements.cards.push(new Card(this.node, element))
+      this.cards.push(new Card(this.node, element))
     })
   }
 }
--- src/dashboard.js
+++ src/dashboard.js
@@ -1,7 +1,9 @@
 import './components/sass/main.scss'
 import { Grid } from './components/grid/grid.js'
 import { NavBar } from './components/navBar/navBar.js'
+import { GridModel } from './model/grid/gridModel.js'
 /* eslint-disable */
 new NavBar('.navbar')
-new Grid('.card', ['1','2','3'])
+new Grid('.card', [{'title':'Title', 'date':'25/12/2018', 'description':'Description Content'}])
+new GridModel()
 /* eslint-enable */
